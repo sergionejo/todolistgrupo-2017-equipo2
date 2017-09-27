cd9f4c5aba46199907c3d14f84ba24c6a8949715
Diego Maroto <diego.diemg@gmail.com>
2017-09-27 10:07:32 +0200

    Refactor control de usuario conectado

--- app/controllers/UsuarioController.java
+++ app/controllers/UsuarioController.java
@@ -6,10 +6,11 @@ import views.html.*;
 import javax.inject.*;
 import play.data.Form;
 import play.data.FormFactory;
+import play.Logger;
 
 import services.UsuarioService;
 import models.Usuario;
-import play.Logger;
+import security.ActionAuthenticator;
 
 public class UsuarioController extends Controller {
 
@@ -33,52 +34,63 @@ public class UsuarioController extends Controller {
          return badRequest(formRegistro.render(form, "Hay errores en el formulario"));
       }
       Registro datosRegistro = form.get();
-      if(usuarioService.findUsuarioPorLogin(datosRegistro.login) != null){
-      	return badRequest(formRegistro.render(form, "Login ya existente: escoge otro"));
+
+      if (usuarioService.findUsuarioPorLogin(datosRegistro.login) != null) {
+         return badRequest(formRegistro.render(form, "Login ya existente: escoge otro"));
       }
-      if(!datosRegistro.password.equals(datosRegistro.confirmacion)){
-      	return badRequest(formRegistro.render(form, "No coinciden la contraseña y la confirmación."));
+
+      if (!datosRegistro.password.equals(datosRegistro.confirmacion)) {
+         return badRequest(formRegistro.render(form, "No coinciden la contraseña y la confirmación"));
       }
       Usuario usuario = usuarioService.creaUsuario(datosRegistro.login, datosRegistro.email, datosRegistro.password);
       return ok(saludo.render("Creado " + usuario.toString()));
    }
 
-   public Result formularioLogin(){
+   public Result formularioLogin() {
       return ok(formLogin.render(formFactory.form(Login.class),""));
    }
 
-   public Result loginUsuario(){
+   public Result loginUsuario() {
       Form<Login> form = formFactory.form(Login.class).bindFromRequest();
-      if (form.hasErrors()){
-         return badRequest(formLogin.render(form, "Hay errores en el formulario."));         
+      if (form.hasErrors()) {
+         return badRequest(formLogin.render(form, "Hay errores en el formulario"));
       }
       Login login = form.get();
       Usuario usuario = usuarioService.login(login.username, login.password);
-      if (usuario == null){
-         return notFound(formLogin.render(form, "Login y contraseña no existentes."));
-      }else{
+      if (usuario == null) {
+         return notFound(formLogin.render(form, "Login y contraseña no existentes"));
+      } else {
+         // Añadimos el id del usuario a la clave `connected` de
+         // la sesión de Play
+         // https://www.playframework.com/documentation/2.5.x/JavaSessionFlash
          session("connected", usuario.getId().toString());
-         return ok(saludo.render("Logeado "+usuario.toString()));
+         return ok(saludo.render("Logeado " + usuario.toString()));
       }
    }
-
-   public Result detalleUsuario(Long id){
-      Usuario usuario = usuarioService.findUsuarioPorId(id);
-      if (usuario == null){
-         return notFound("Usuario no encontrado");
-      }else {
-         Logger.debug("Encontrado usuario " + usuario.getId() + ": " + usuario.getLogin());
-         return ok(detalleUsuario.render(usuario));
-      }
-   }
-
-   public Result logout(){
+   
+   // Comprobamos si hay alguien logeado con @Security.Authenticated(ActionAuthenticator.class)
+   // https://alexgaribay.com/2014/06/15/authentication-in-play-framework-using-java/
+   @Security.Authenticated(ActionAuthenticator.class)
+   public Result logout() {
       String connectedUserStr = session("connected");
-      if (connectedUserStr != null){
-         session().remove("connected");
-         return ok(saludo.render("Adión usuario "+ connectedUserStr));
-      }else{
-         return ok(saludo.render("No hay ningún usuario conectado"));
+      session().remove("connected");
+      return ok(saludo.render("Adios usuario " + connectedUserStr));
+   }
+
+   @Security.Authenticated(ActionAuthenticator.class)
+   public Result detalleUsuario(Long id) {
+      String connectedUserStr = session("connected");
+      Long connectedUser =  Long.valueOf(connectedUserStr);
+      if (connectedUser != id) {
+         return unauthorized("Lo siento, no estás autorizado");
+      } else {
+         Usuario usuario = usuarioService.findUsuarioPorId(id);
+         if (usuario == null) {
+            return notFound("Usuario no encontrado");
+         } else {
+            Logger.debug("Encontrado usuario " + usuario.getId() + ": " + usuario.getLogin());
+            return ok(detalleUsuario.render(usuario));
+         }
       }
    }
 }
