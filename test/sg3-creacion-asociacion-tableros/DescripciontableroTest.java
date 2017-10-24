4f105067cd983fa3725afd844fa58a58eb934026
Diego Maroto <diego.diemg@gmail.com>
2017-10-24 18:30:26 +0200

    Conseguir el listado de participantes de un tablero.

---
+++ test/sg3-creacion-asociacion-tableros/DescripciontableroTest.java
@@ -0,0 +1,72 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import play.db.jpa.*;
+
+import org.dbunit.*;
+import org.dbunit.dataset.*;
+import org.dbunit.dataset.xml.*;
+import org.dbunit.operation.*;
+import java.io.FileInputStream;
+
+import java.util.List;
+
+import models.Usuario;
+import models.Tablero;
+
+import play.inject.guice.GuiceApplicationBuilder;
+import play.inject.Injector;
+import play.inject.guice.GuiceInjectorBuilder;
+import play.Environment;
+
+import services.UsuarioService;
+import services.UsuarioServiceException;
+import services.TableroService;
+import services.TableroServiceException;
+
+public class DescripciontableroTest {
+    static private Injector injector;
+
+    @BeforeClass
+    static public void initApplication() {
+        GuiceApplicationBuilder guiceApplicationBuilder =
+        new GuiceApplicationBuilder().in(Environment.simple());
+        injector = guiceApplicationBuilder.injector();
+        injector.instanceOf(JPAApi.class);
+    }
+
+    @Before
+    public void initData() throws Exception {
+        JndiDatabaseTester databaseTester = new JndiDatabaseTester("DBTest");
+        IDataSet initialDataSet = new FlatXmlDataSetBuilder().build(new FileInputStream("test/resources/usuarios_dataset.xml"));
+        databaseTester.setDataSet(initialDataSet);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.onSetup();
+    }
+
+    private TableroService newTableroService() {
+        return injector.instanceOf(TableroService.class);
+    }
+    
+   private UsuarioService newUsuarioService() {
+      return injector.instanceOf(UsuarioService.class);
+   }
+
+    @Test
+    public void checkParticipantes() {
+        TableroService tableroService = newTableroService();
+        UsuarioService usuarioService = newUsuarioService();
+        Usuario administrador = usuarioService.findUsuarioPorId(1000L);
+        Usuario participante = usuarioService.findUsuarioPorId(1001L);
+        Tablero tablero1 = new Tablero(administrador, "Tablero 1");
+        assertTrue(tablero1.addParticipante(participante));
+        Usuario usuario1 = usuarioService.creaUsuario("usuario1", "usuario1@gmail.com","1234");
+        Usuario usuario2 = usuarioService.creaUsuario("usuario2", "usuario2@gmail.com","1234");
+        Usuario usuario3 = usuarioService.creaUsuario("usuario3", "usuario3@gmail.com","1234");
+        assertTrue(tablero1.addParticipante(usuario1));
+        assertTrue(tablero1.addParticipante(usuario2));
+        assertTrue(tablero1.addParticipante(usuario3));
+        tablero1 = tableroService.updateTablero(tablero1);
+        assertEquals(4,tableroService.allParticipantesTablero(tablero1.getId()).size());
+    }
+}
